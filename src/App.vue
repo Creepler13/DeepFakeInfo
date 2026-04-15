97a95298d2eccac9fe3ea2221ba7e0c4a619ec79
mk <mika.bensien@gnail.com>
2026-04-15 13:47:13 +0200

    tailwilnd implementation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="bg-red-400 font-xl bg-red-400">
-    <div>Hallo Toller Cooller Super Duper Test</div>
+  <div class="bg-red-400">
+    <div class="text-xl">Hallo Toller Cooller Super Duper Test</div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
