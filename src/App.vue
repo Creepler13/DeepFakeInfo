b7480e90eaad0ae3c4f72529d458375630eb4e71
Creepler13 <denis.schlichting03@gmail.com>
2026-04-20 17:55:17 +0200

    uitest

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="bg-gray-800 w-svw h-svh text-gray-50 font-medium p-5">
-    <div class="text-5xl text-center">Deepfakes</div>
-    <div class="w-full h-[3px] my-5 bg-gray-400 rounded-full"></div>
-    <nav>
-      <RouterLink to="/video">Go to Video</RouterLink>
-      </nav>
         <RouterView />
 
   </div>
--- src/views/quiz.vue
+++ src/views/quiz.vue
@@ -1,10 +1,24 @@
 <template>
 
     <div>
-        <img v-if="$data.currentQuestion == 0" src="../assets/media/quiz/monroe.webp" />
-        <img v-if="$data.currentQuestion == 1" src="../assets/media/quiz/frage09_01.jpg" />
-
+        <img v-if="$data.currentQuestion == 0" src="../assets/media/quiz/frage10_01.png" />
+        <img v-if="$data.currentQuestion == 1" src="../assets/media/quiz/frage14_01.jpg" />
+        <img v-if="$data.currentQuestion == 2" src="../assets/media/quiz/frage15_02.jpg" />
+        <img v-if="$data.currentQuestion == 3" src="../assets/media/quiz/frage12_01.png" />
+        <img v-if="$data.currentQuestion == 4" src="../assets/media/quiz/frage06_01.jpg" />
+        <img v-if="$data.currentQuestion == 5" src="../assets/media/quiz/frage04_02.jpg" />
+        <img v-if="$data.currentQuestion == 6" src="../assets/media/quiz/frage16_01.jpg" />
+        <img v-if="$data.currentQuestion == 7" src="../assets/media/quiz/frage07_01.jpg" />
+        <img v-if="$data.currentQuestion == 8" src="../assets/media/quiz/frage05_01.jpg" />
+        <img v-if="$data.currentQuestion == 9" src="../assets/media/quiz/frage03_02.jpg" />
     </div>
+    <div class="flex justify-around mt-3">
+    <div>
+        <button>Real</button>
+    </div>
+     <div>
+        <button>Fake</button>
+    </div></div>
 </template>
 
 
@@ -17,7 +31,9 @@ export default {
         return {
             currentQuestion: 0,
             points: 0,
-
+            questions: [
+                true, false, true, true, true, false, true, true, true, false
+            ]
         }
     }
 }
