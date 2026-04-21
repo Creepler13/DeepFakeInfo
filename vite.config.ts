3c7503533e7ba13c9afb771abaffd403104e7f5e
Creepler13 <denis.schlichting03@gmail.com>
2026-04-21 08:06:29 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 import vuetify from "vite-plugin-vuetify"
-import bundlesize from "vite-plugin-bundlesize";
+
 
 export default defineConfig({
   base: "/DeepFakeInfo/",
   plugins: [vue(),
   vueDevTools(),
   vuetify({ autoImport: true }),
-  bundlesize()],
+ ],
   assetsInclude: ['**/*.txt'],
   resolve: {
     alias: {
